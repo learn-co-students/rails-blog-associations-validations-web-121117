1e69b68d96ea678962e2e21fb18a71038041dd4e
julientregoat <julienctregoat@gmail.com>
2018-01-21 13:23:32 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
-  
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  belongs_to :user
+  validates :name, presence: true
+  validates :content, presence: true
+
+  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+
 end
